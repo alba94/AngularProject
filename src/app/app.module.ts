5debaaac27383f02f87ecf2e8b75a4442c94d317
Alba Llubani <allubani@ikub.local>
2018-06-05 10:05:45 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/index/app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ClientComponent } from './components/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule
